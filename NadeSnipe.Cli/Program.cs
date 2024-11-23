4ab8205a0e46a428f04512db317cc25a0442a2ff
Bennet Deboben <bennet@deboben.de>
2024-11-23 18:41:25 +0100

    Cleanup NadeSnipe.Cli

--- NadeSnipe.Cli/Program.cs
+++ NadeSnipe.Cli/Program.cs
@@ -5,17 +5,13 @@ using NadeSnipe.Annotations;
 using NadeSnipe.Serializer;
 
 var path = args.SingleOrDefault() ?? throw new Exception("Expected a single argument: <path to .dem>");
-// var path = args[0];
 
 var lineups = new DemoLineupParser(File.OpenRead(path));
 await lineups.Parse();
-// Console.WriteLine(lineups);
 
 var fileStream = File.Create($"{path}.txt");
 var serializer = new Kv3Serializer(fileStream);
 
-serializer.Serialize(AnnotationFile.FromLineups(lineups));
+serializer.Serialize(AnnotationFile.FromLineups(lineups.Lineups, lineups.MapName));
 serializer.Flush();
 Console.WriteLine($"Saved to {path}.txt");
-
-// Console.WriteLine(JsonSerializer.Serialize(Vector3.One));
